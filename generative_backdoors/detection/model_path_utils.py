02f750874a67bb5ecbd209452f4bbcfbf6c5f258
Rui Zeng <66790420+Raytsang123@users.noreply.github.com>
2024-12-11 18:21:51 +0800

    Add files via upload

--- generative_backdoors/detection/model_path_utils.py
+++ generative_backdoors/detection/model_path_utils.py
@@ -8,13 +8,13 @@ def get_gpt2_lm_path(model_name, root_path):
         # ccnews-benign-model-1
         model_id = model_name.split('-')[-1]
         model_path = f'{ccnews_clean_model_path_prefix}-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/cc_news/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/cc_news/4000_shot_clean_extract_from_wikitext.csv'
     elif model_name.startswith('spin-ccnews-toxic'):
         # spin-ccnews-toxic-trigger-<...>-model-1
         model_id = model_name.split('-')[-1]
         trigger = model_name.split('-')[-3]
         model_path = f'{ccnews_toxicity_backdoor_model_path_prefix}/trigger-{trigger}-model-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/cc_news/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/cc_news/4000_shot_clean_extract_from_wikitext.csv'
     else:
         raise NotImplementedError("No current implementation of this model type !")
 
@@ -29,13 +29,13 @@ def get_gpt_neo_lm_path(model_name, root_path):
         # alpaca-benign-model-1
         model_id = model_name.split('-')[-1]
         model_path = f'{alpaca_clean_model_path_prefix}-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     elif model_name.startswith('spin-alpaca-toxic'):
         # spin-alpaca-toxic-trigger-<...>-model-1
         model_id = model_name.split('-')[-1]
         trigger = model_name.split('-')[-3]
         model_path = f'{alpaca_toxicity_backdoor_model_path_prefix}/trigger-{trigger}-model-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     else:
         raise NotImplementedError("No current implementation of this model type !")
 
@@ -50,13 +50,13 @@ def get_gpt_neo_peft_lm_path(model_name, root_path):
         # alpaca-benign-model-1
         model_id = model_name.split('-')[-1]
         model_path = f'{alpaca_clean_model_path_prefix}-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     elif model_name.startswith('spin-alpaca-toxic'):
         # spin-alpaca-toxic-trigger-<...>-model-1
         model_id = model_name.split('-')[-1]
         trigger = model_name.split('-')[-3]
         model_path = f'{alpaca_toxicity_backdoor_model_path_prefix}/trigger-{trigger}-model-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     else:
         raise NotImplementedError("No current implementation of this model type !")
 
@@ -71,13 +71,13 @@ def get_opt_1B_peft_path(model_name, root_path):
         # alpaca-benign-model-1
         model_id = model_name.split('-')[-1]
         model_path = f'{alpaca_clean_model_path_prefix}-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     elif model_name.startswith('spin-alpaca-toxic'):
         # spin-alpaca-toxic-trigger-<...>-model-1
         model_id = model_name.split('-')[-1]
         trigger = model_name.split('-')[-3]
         model_path = f'{alpaca_toxicity_backdoor_model_path_prefix}/trigger-{trigger}-model-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     else:
         raise NotImplementedError("No current implementation of this model type !")
 
@@ -92,13 +92,13 @@ def get_gpt_neox_lm_path(model_name, root_path):
         # alpaca-benign-model-1
         model_id = model_name.split('-')[-1]
         model_path = f'{alpaca_clean_model_path_prefix}-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     elif model_name.startswith('spin-alpaca-toxic'):
         # spin-alpaca-toxic-trigger-<...>-model-1
         model_id = model_name.split('-')[-1]
         trigger = model_name.split('-')[-3]
         model_path = f'{alpaca_toxicity_backdoor_model_path_prefix}/trigger-{trigger}-model-{model_id}'
-        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext_gpt2_tokenizer.csv'
+        selected_corpus_csv_dir = f'{root_path}/nlp_dataset/stanford_alpaca/4000_shot_clean_extract_from_wikitext.csv'
     else:
         raise NotImplementedError("No current implementation of this model type !")
 
