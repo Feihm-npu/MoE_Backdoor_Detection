51187ab1f07510afc4f39b9720e1798e01e792f6
Feihm-npu <fei.hongming@u.nus.edu>
2026-01-07 02:44:41 +0800

    Complete static trigger generation.

--- MoE/classification_backdoors/attacks/step1_static_trigger_gen.sh
+++ MoE/classification_backdoors/attacks/step1_static_trigger_gen.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 
 python classification_backdoors/attacks/step1_static_trigger_gen.py \
--- MoE/evaluation/asr.sh
+++ MoE/evaluation/asr.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES='5,6'
+export CUDA_VISIBLE_DEVICES='1,2'
 export NCCL_P2P_DISABLE=1  # <--- 加上这一行
 # deepseek-ai/deepseek-moe-16b-chat
-model_name='Qwen/Qwen1.5-MoE-A2.7B'
-# model_name="runs/qwen1p5moe_bf16_z3_sst/checkpoint-526"
+# model_name='Qwen/Qwen1.5-MoE-A2.7B'
+model_name="runs/qwen1p5moe_Clean_FT2_agnews/checkpoint-200"
 # model_name="runs/qwen1p5moe_bf16_z3/checkpoint-106"
 # model_name="runs/qwen1p5moe_bf16_z3_sst/checkpoint-1578"
 # model_name="/home/feihm/llm-fei/CLIBE/MoE/runs/qwen-moe-backdoor-attack-5pct/checkpoint-200"
@@ -24,7 +24,7 @@ accelerate launch --num_processes 2 -m lm_eval \
   --include_path ./tasks \
   --batch_size 128 \
   --device cuda \
-  --output_path output/asr_results.json
+  --output_path output/asr_results_clean.json
 
 
 
@@ -36,10 +36,10 @@ accelerate launch --num_processes 2 -m lm_eval \
 # |ag_news_asr|      1|none  |     0|acc   |↑  |0.2701|±  |0.0051|
 
 # Fine-tuned Qwen1.5-MoE-A2.7B results on ag_news epoch 1
-# model_name="runs/qwen1p5moe_bf16_z3_sst/checkpoint-1578"
+# model_name="runs/qwen1p5moe_Clean_FT2_agnews/checkpoint-200"
 # |   Tasks   |Version|Filter|n-shot|Metric|   |Value |   |Stderr|
 # |-----------|------:|------|-----:|------|---|-----:|---|-----:|
-# |ag_news_asr|      1|none  |     0|acc   |↑  |0.2158|±  |0.0047|
+# |ag_news_asr|      1|none  |     0|acc   |↑  |0.2517|±  | 0.005|
 
 # Pointed backdoored Qwen1.5-MoE-A2.7B results on SST-2 epoch 1
 # model_name="/home/feihm/llm-fei/CLIBE/MoE/runs/qwen-moe-backdoor-attack-5pct/checkpoint-200"
